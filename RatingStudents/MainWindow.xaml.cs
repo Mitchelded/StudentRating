53f28432b7b5f7a2b8e43f52521d7c54112764b9
Mitchelded <tretyakovanton0000@gmail.com>
2024-03-24 19:18:09 +0300

    first commit

---
+++ RatingStudents/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+
+namespace RatingStudents;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void BtnRun_OnClick(object sender, RoutedEventArgs e)
+    {
+        
+        if (String.IsNullOrEmpty(TbPort.Text) && String.IsNullOrEmpty(PbPassword.Password))
+        {
+            MessageBox.Show("Port & Password Has Not Been Specified", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        } 
+        else if (String.IsNullOrEmpty(TbPort.Text) || String.IsNullOrEmpty(PbPassword.Password))
+        {
+            MessageBox.Show($"{(String.IsNullOrEmpty(TbPort.Text) ? "Port" : "Password")} Has Not Been Specified", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+
+        // if (PbPassword.Password == null)
+        // {
+        //     MessageBox.Show("Password Has Not Been Specified", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        // }
+    }
+}
---
+++ RatingStudents/Window Students.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows;
+
+namespace RatingStudents;
+
+public partial class Window_Students : Window
+{
+    public Window_Students()
+    {
+        InitializeComponent();
+    }
+}
